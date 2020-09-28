d6952aaab7b2fa348b6a22e98349afcc90a4e74f
Liliana Weimer <lilianaweimer11@gmail.com>
2020-09-28 12:39:11 -0600

    Add conditions to check for ghosts adjacent to squares

--- app.js
+++ app.js
@@ -22,7 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     //add numbers to squares indicating nearby ghosts
-  for (let i = 0; )    
+  for (let i = 0; i < squares.length; i++) {
+    let total = 0;
+    const isLeftEdge = (i % width === 0);
+    const isRightEdge = (i % width === width - 1);
+
+    if (squares[i].classList.contains('safe')) {
+      if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains('ghost')) total++; 
+      if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('ghost')) total++;
+      if (i > 10 && squares[i - width].classList.conatins('ghost')) total++;
+      if (i > 11 && !isLeftEdge && squares[i - 1 - width].classList.contains('ghost')) total++;
+      if (i < 98 && !isRightEdge && squares[i + 1].classList.contains('ghost')) total++;
+      if (i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('ghost')) total++;
+      if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('ghost')) total++;
+      if (i < 89 && squares[i + width].classList.conatins('ghost')) total++;
+      squares[i].setAttribute('data', total);
+      console.log(squares);
+    }
+  }    
 
   }
 
